8d85e4bd4d2418d4d0560908b9b83922d7b13c14
maurya512 <patelmaurya0512@gmail.com>
2021-11-22 23:48:35 -0500

    Added styling to the navbar, added images for the web app and more

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+// import icons
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
+import logo from "../../assets/logo.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT 3</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
     </div>
   );
 };
