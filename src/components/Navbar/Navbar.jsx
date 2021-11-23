46ff83adbcac9fe570caed8f012a569c0102bfa2
maurya512 <patelmaurya0512@gmail.com>
2021-11-23 00:16:53 -0500

    Added styling to the navbar and buttons by following the figma files

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,6 +19,10 @@ const Navbar = () => {
           <p><a href="#blog">Library</a></p>
         </div>
       </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
     </div>
   );
 };
