45829b3b663de92dd72fa4ec29b91430534c2fa7
maurya512 <patelmaurya0512@gmail.com>
2021-11-24 22:51:03 -0500

    Added menu option to the navbar and state functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 // import icons
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./navbar.css";
 import logo from "../../assets/logo.svg";
 
+// functional component that can be reused whenever we need it
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt3">What is GPT 3</a>
+    </p>
+    <p>
+      <a href="#possibility">Possibility</a>
+    </p>
+    <p>
+      <a href="#features">Features</a>
+    </p>
+    <p>
+      <a href="#blog">Blog</a>
+    </p>
+  </>
+);
+
 const Navbar = () => {
+  // check the state of the application
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -12,16 +35,35 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT 3</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <Menu />
         </div>
       </div>
       <div className="gpt3__navbar-sign">
         <p>Sign In</p>
         <button type="button">Sign Up</button>
+      </div>
+      {/* mobile responsive section of the navbar */}
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
