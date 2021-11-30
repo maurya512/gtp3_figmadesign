fde84b83de2c4290a8bffd09114eaa733c4e28d9
maurya512 <patelmaurya0512@gmail.com>
2021-11-29 23:41:13 -0500

    added styling to the brand component

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -10,9 +10,17 @@ const Brand = () => {
       {/* div for all images */}
       <div>
         <img src={google} alt="google" />
+      </div>
+      <div>
         <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
         <img src={slack} alt="slack" />
+      </div>
+      <div>
         <img src={shopify} alt="shopify" />
+      </div>
+      <div>
         <img src={atlassian} alt="atlassian" />
       </div>
     </div>
