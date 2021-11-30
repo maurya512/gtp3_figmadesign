8fcb9e403c926f1a8e44c49e2bac76322369ada3
maurya512 <patelmaurya0512@gmail.com>
2021-11-30 10:22:33 -0500

    created and styled whatgpt3 component using angry tools, variables and media queries

--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Feature } from "../../components";
 import "./WhatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div>
-      <h1>What GPT 3</h1>
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        {/* Feature component created before will be reused at many different places */}
+        <Feature />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        {/* classname implemented in app.css */}
+        <h1 className="gradient__text">
+          Your possibilities are beyond your imagination
+        </h1>
+        <p>Explore the library</p>
+      </div>
+      {/* Feature component that will contain 3 features */}
+      <div className="gpt3__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   );
 };
