6ea6c4382303dc3597d8ef4928cb0113b57694c2
maurya512 <patelmaurya0512@gmail.com>
2021-11-29 23:35:48 -0500

    created a helper file to import all the images and created a brand component

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import './brand.css';
+import React from "react";
+import "./brand.css";
+
+// import images from the helper file
+import { google, atlassian, dropbox, shopify, slack } from "./import";
 
 const Brand = () => {
-    return (
-        <div>
-           <h1>Brand</h1> 
-        </div>
-    )
-}
+  return (
+    <div className="gpt3__brand section__padding">
+      {/* div for all images */}
+      <div>
+        <img src={google} alt="google" />
+        <img src={dropbox} alt="dropbox" />
+        <img src={slack} alt="slack" />
+        <img src={shopify} alt="shopify" />
+        <img src={atlassian} alt="atlassian" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
---
+++ src/components/Brand/import.js
@@ -0,0 +1,11 @@
+// helper file to import all the required images to be used in the brand section
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+// export all the images 
+export {
+    google, slack, atlassian, dropbox, shopify
+}
