5d56e478c84889cca42b2d73d157c2f47cc88ee7
maurya512 <patelmaurya0512@gmail.com>
2021-11-30 23:22:51 -0500

    created a feature component and passed data through it using props

--- src/containers/WhatGPT3/WhatGPT3.jsx
+++ src/containers/WhatGPT3/WhatGPT3.jsx
@@ -7,7 +7,11 @@ const WhatGPT3 = () => {
     <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
         {/* Feature component created before will be reused at many different places */}
-        <Feature />
+        {/* define the props for feature component which can be later accessed by the component as needed */}
+        <Feature
+          title="What is GPT 3"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
       </div>
       <div className="gpt3__whatgpt3-heading">
         {/* classname implemented in app.css */}
@@ -18,9 +22,18 @@ const WhatGPT3 = () => {
       </div>
       {/* Feature component that will contain 3 features */}
       <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. "
+        />
+        <Feature
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+        <Feature
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
       </div>
     </div>
   );
