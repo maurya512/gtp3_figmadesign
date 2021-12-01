065b6a7d279e86c5e0127c65bbac22cf491c9ed5
maurya512 <patelmaurya0512@gmail.com>
2021-11-30 23:37:49 -0500

    added styling to the feature component

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./feature.css";
 
-// Feature component accepts prompts that can be passed 
+// Feature component accepts prompts that can be passed
 const Feature = ({ title, text }) => {
   return (
     <div className="gpt3__features-container__feature">
       <div className="gpt3__features-container__feature-title">
+        {/* the plain div that will be used to style the line above h1 */}
         <div />
         <h1>{title}</h1>
-        <div className="gpt3__features-container_feature-text">{text}</div>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
       </div>
     </div>
   );
