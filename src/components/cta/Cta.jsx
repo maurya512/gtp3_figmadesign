d138cce64e5c05b53412888bab206c9569d02df3
maurya512 <patelmaurya0512@gmail.com>
2021-12-02 23:10:39 -0500

    Created and styled the cta component

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import './cta.css';
+import React from "react";
+import "./cta.css";
 const Cta = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access</p>
+        <h3>Register today & start exploring the endless possibilites.</h3>
+      </div>
+      {/* button to register for early access */}
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default Cta
+export default Cta;
