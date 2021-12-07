4f9c129a51f8d06de12891399f2f9a034a260ac9
maurya512 <patelmaurya0512@gmail.com>
2021-12-06 23:14:23 -0500

    styled and designed a footer component

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,12 +1,55 @@
-import React from 'react'
-import './footer.css';
+import React from "react";
+import "./footer.css";
+
+// import images
+import gpt3Logo from "../../assets/logo.svg";
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>Footer</h1>
+  return (
+    <div className="gpt3__footer section__padding">
+      {/* heading of the footer */}
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step into the future before others
+        </h1>
+      </div>
+      {/* footer button */}
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      {/* footer branding */}
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="logo" />
+          <p>Crechterwoord K12 182 DK, All Rights Reserved</p>
         </div>
-    )
-}
+        {/* footer branding other */}
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <p>Overons</p>
+          <p>Social Media</p>
+          <p>Counters</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <p>Terms and Conditions</p>
+          <p>Privacy Policy</p>
+          <p>Contact</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in Touch</h4>
+          <p>123 North Pine Ave. Cedarville, CA 85954</p>
+          <p>085-965-7485</p>
+          <p>info@gpt3.com</p>
+        </div>
+      </div>
+      {/* all rights reserved */}
+      <div className="gpt3__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
